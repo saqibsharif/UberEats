52214d14ffdad50574cd5be97feb416c49bb4be3
Saqib Sharif <sp17-bse-094@isbstudent.comsats.edu.pk>
2022-07-07 04:43:28 +0500

    The about component is now dynamic and gets data from the parent component instead of hardcoded data

--- App.js
+++ App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import Home from './screens/Home';
-import RestaurantDetail from './screens/RestaurantDetail';
+import RootNavigation from './navigation';
 
 export default function App() {
-  return <RestaurantDetail/>;
+  return <RootNavigation/>;
 }
