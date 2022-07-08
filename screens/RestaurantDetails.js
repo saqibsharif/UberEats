a372e8ebef36ffd2e113348bada2d73e2c848881
Saqib Sharif <sp17-bse-094@isbstudent.comsats.edu.pk>
2022-07-08 20:56:57 +0500

    Added the View Cart button in Restaurant Details Screen

--- screens/RestaurantDetails.js
+++ screens/RestaurantDetails.js
@@ -3,20 +3,24 @@ import {View, StyleSheet} from "react-native";
 import About from "../components/restaurantDetails/About";
 import {Divider} from "react-native-elements";
 import MenuItems from "../components/restaurantDetails/MenuItems";
+import ViewCart from "../components/restaurantDetails/ViewCart";
 
-export default function RestaurantDetails({route}){
+export default function RestaurantDetails({route, navigation}){
     return(
+        <>
         <View>
         {/* Getting the restaurant details from route passed from RestaurantItem component in HomeScreen and passing it to About Component */}
         <About route={route}/>
         <Divider width={1.8} style={styles.Divider}/>
         <MenuItems/>
         </View>
+        <ViewCart navigation={navigation} restaurantName={route.params.name} />
+        </>
     );
 }
 
 const styles = StyleSheet.create({
    Divider:{
     marginVertical: 20,
-   } 
+   },
 });
